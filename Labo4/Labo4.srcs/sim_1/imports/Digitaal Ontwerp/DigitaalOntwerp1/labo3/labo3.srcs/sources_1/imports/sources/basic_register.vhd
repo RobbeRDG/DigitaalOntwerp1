745b074eaf9dbd9d0f7ac480d21558a6e91f40b1
RobbeRDG <robbedegroeve@gmail.com>
2020-10-22 16:24:15 +0200

    started labo 4

---
+++ Labo4/Labo4.srcs/sim_1/imports/Digitaal Ontwerp/DigitaalOntwerp1/labo3/labo3.srcs/sources_1/imports/sources/basic_register.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Institution: KU Leuven
+-- Students: firstname lastname and other guy/girl/...
+-- 
+-- Module Name: basic_register - Behavioral
+-- Course Name: Lab Digital Design
+-- 
+-- Description: 
+--  A "standard" n-bit register with asycnhronous reset and synchronous load,
+--  using a "load enable" signal (le).
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity basic_register is
+    generic(
+        C_DATA_WIDTH : natural := 8
+    );
+    port(
+                 clk : in  std_logic;  -- clock signal
+               reset : in  std_logic;  -- async. reset
+                  le : in  std_logic;  -- synch. load
+             data_in : in  std_logic_vector(C_DATA_WIDTH-1 downto 0); -- n-bit data in
+            data_out : out std_logic_vector(C_DATA_WIDTH-1 downto 0)  -- n-bit register output
+    );
+end basic_register;
+
+architecture Behavioral of basic_register is
+    -- TODO: (optionally) declare signals
+    signal zero_sig : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others=>'0');
+--    signal previous_data_out_sig : std_logic_vector(C_DATA_WIDTH-1 downto 0) := (others=>'0');
+--    signal data_out_sig : std_logic_vector(C_DATA_WIDTH-1 downto 0);
+ 
+    
+    
+begin
+      
+    -- TODO: write VHDL process
+    process (clk, reset) begin
+        if reset = '1' then
+            data_out <= zero_sig;
+        elsif rising_edge(clk) and le = '1' then
+            data_out <= data_in;
+        end if;
+
+        
+   end process;
+end Behavioral;
